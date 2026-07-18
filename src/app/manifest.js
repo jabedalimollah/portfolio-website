358a1cf8a38c678dcf8fe100faa7e6e2bb17631b
sarif <iamshadowknight3@gmail.com>
2026-07-19 01:35:57 +0530

    fix manifest.js file

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -18,13 +18,13 @@ export default function manifest() {
 
     icons: [
       {
-        src: "/logo/logo-192x192.png",
+        src: "/logo/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
 
       {
-        src: "/logo/logo-512x512.png",
+        src: "/logo/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
